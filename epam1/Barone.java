6505c6c45f4f8c69b664ef966687618013671c56
yogeshwarreddy <60438341+yogeshwarreddy460@users.noreply.github.com>
2020-07-24 10:16:14 +0530

    first commit

---
+++ epam1/Barone.java
@@ -0,0 +1,8 @@
+package com.epam.example.chocolates;
+
+public class Barone extends Chocolate {
+
+    public Barone(String name, int price, int weight) {
+        super(name, price, weight);
+    }
+}
---
+++ epam1/Cadbury.java
@@ -0,0 +1,8 @@
+package com.epam.example.chocolates;
+
+public class Cadbury extends Chocolate {
+
+    public Cadbury(String name,int price,int weight){
+        super(name,price,weight);
+    }
+}
